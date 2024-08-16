d2d0f81b71da54260b71b23c260a434d706dba0e
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-08-17 01:19:14 +0530

    matchScheme

--- functions/redirector/lib/main.dart
+++ functions/redirector/lib/main.dart
@@ -1,6 +1,18 @@
 import 'dart:async';
 import 'dart:io';
 
+extension ListExtension on List<String> {
+  bool matchesScheme(String scheme) {
+    if (this.isEmpty) return false;
+    for (final item in this) {
+      if (item.contains(scheme)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
 /// Redirector Function
 /// Redirects you to your Native App
 /// Used for resetting password, verifying email, etc.
@@ -27,7 +39,6 @@ Future<dynamic> main(final context) async {
       final secret = queryParams['secret'] as String?;
       final userId = queryParams['userId'] as String?;
       final expire = queryParams['expire'] as String?;
-
       if (scheme == null || secret == null || userId == null || expire == null) {
         context.log('Missing Query Params: {scheme: $scheme, secret: $secret, userId: $userId, expire: $expire}');
         return context.res.send('Missing Query Params', 400, {
@@ -36,7 +47,7 @@ Future<dynamic> main(final context) async {
       }
       final decodedScheme = Uri.decodeComponent(scheme);
 
-      if (!validSchemes.contains(decodedScheme)) {
+      if (!validSchemes.matchesScheme(decodedScheme)) {
         context.log('Invalid Scheme. $scheme');
         return context.res.send('Invalid Scheme. If its correct, please add in your env in your cloud function', 400, {
           'content-type': 'text/plain',
